cb9ce8cfbc2ff2cb559ca2e18b7b994683c957bc
Mads Skovgaard <madsskovgaard@hotmail.com>
2019-03-21 23:20:10 +0100

    Adds window icon and fixes pack dependencies.

--- public/electron.js
+++ public/electron.js
@@ -20,7 +20,8 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: false,
       preload: __dirname + '/preload.js'
-    }
+    },
+    icon: __dirname + '/favicon.ico'
   });
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
