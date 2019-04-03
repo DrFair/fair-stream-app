c85dd48114972380c3d531d1b771aa744d741d0f
Mads Skovgaard <madsskovgaard@hotmail.com>
2019-04-03 22:20:47 +0200

    Changes example overlay to display timed notifications.

--- overlays/example/main.js
+++ overlays/example/main.js
@@ -7,8 +7,41 @@
 
   onNotification(function(data) {
     console.log(data);
-    document.body.innerHTML += `<p>${data.systemMsg ? data.systemMsg : data.msg}</p>`;
+    var content = '';
+    switch(data.event) {
+      case 'sub': {
+        var tier = data.tier === 'Prime' ? 'Twitch Prime' : 'Tier ' + data.tier.charAt(0);
+        content = `${data.displayName} subbed with ${tier}!`;
+        break;
+      }
+      case 'resub': {
+        var tier = data.tier === 'Prime' ? 'Twitch Prime' : 'Tier ' + data.tier.charAt(0);
+        content = `${data.displayName} resubbed with ${tier} x${data.months}!`;
+        break;
+      }
+      case 'giftsub': {
+        var tier = data.tier === 'Prime' ? 'Twitch Prime' : 'Tier ' + data.tier.charAt(0);
+        content = `${data.displayName} gifted a ${tier} sub to ${data.recepient.displayName}!`;
+        break;
+      }
+      case 'massgiftsub': {
+        var tier = data.tier === 'Prime' ? 'Twitch Prime' : 'Tier ' + data.tier.charAt(0);
+        content = `${data.displayName} gifted ${data.massCount} ${tier} subs!`;
+        break;
+      }
+      case 'bits': {
+        content = `${data.displayName} cheered ${data.bits} bits!`;
+        break;
+      }
+      default: {
+        content = `${data.displayName} ${data.event}`;
+        break;
+      }
+    }
+    document.getElementById('notifications').innerHTML = content;
     setTimeout(function() {
+      // Clear notifications
+      document.getElementById('notifications').innerHTML = '';
       console.log('Ready next!');
       data.requestNext();
     }, 5000);
